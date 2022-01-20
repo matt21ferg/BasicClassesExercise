585920f4c32ec3d71244f2b17e95b73c524b47ab
matt21ferg <matt21ferguson@gmail.com>
2022-01-20 11:01:52 -0600

    1st commit

---
+++ BasicClassesExercise/Cars.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicClassesExercise
+{
+   public class Cars
+    {
+        public Cars()
+        {
+                
+        }
+        public string make { get; set; } 
+        public string model { get; set; }
+        public int year { get; set; } 
+
+        
+    }
+}
---
+++ BasicClassesExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BasicClassesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Cars();
+            myCar.model = "Jimmy";
+            myCar.make = "GMC";
+            myCar.year = 1998;
+            Console.WriteLine($"{ myCar.model} {myCar.year} { myCar.make}");
+            
+        }
+    }
+}
